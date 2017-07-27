14b5c807a8084fd6bc27ba43006e3d65e19daf53
Leonardo Santos <leonardossev@gmail.com>
2017-07-27 09:51:50 -0300

    Atualiza links e adiciona imagem ao footer

--- ProjetoMGov/assets/includes/footer.php
+++ ProjetoMGov/assets/includes/footer.php
@@ -6,7 +6,6 @@
                 <div class="ui inverted link list">
                     <a class="item" href="https://www.facebook.com/mgovbr/" target="_blank"><i class="facebook icon"></i></i:>Facebook</a>
                     <a class="item" href="https://www.linkedin.com/company-beta/10858808/" target="_blank"><i class="linkedin icon"></i>LinkedIn</a>
-                    <a class="item" href="http://www.mgovbrasil.com.br/" target="_blank"><i class="linkedin icon"></i>LinkedIn</a>
                 </div>
             </div>
             <div class="eight wide right floated column">
@@ -15,7 +14,9 @@
             </div>
         </div>
         <div class="ui inverted section divider"></div>
-        <img src="assets/img/mgov.pmg" class="ui centered mini image">
+        <a href="http://www.mgovbrasil.com.br/" target="_blank">
+            <img src="assets/img/mgov.png" class="ui centered small image">
+        </a>
         <div class="ui horizontal inverted small divided link list">
             &copy; 2017 - Direitos reservados
         </div>
