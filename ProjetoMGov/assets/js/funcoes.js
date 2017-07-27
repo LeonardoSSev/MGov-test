5e62680e6ed3bd44bb910b69c446d1d557506d2d
Leonardo Santos <leonardossev@gmail.com>
2017-07-27 09:26:44 -0300

    Adiciona formulário e métodos de gerenciamento de formulário

--- ProjetoMGov/assets/js/funcoes.js
+++ ProjetoMGov/assets/js/funcoes.js
@@ -1,3 +1,31 @@
+function buscarMetodo(){
+    var metodo = document.getElementById("busca");
+    var metodoSelecionado = metodo.options[metodo.selectedIndex].value;
+    document.getElementById("teste").innerHTML = metodoSelecionado;
+    if(metodoSelecionado == "") {
+        document.getElementById("searchButton").className="ui teal disabled button";
+    } else{
+        document.getElementById("searchButton").className="ui teal button";
+        if(metodoSelecionado == "nome"){
+            document.getElementById("inputNome").type="text";
+            document.getElementById("inputCPF").type="hidden";
+            document.getElementById("inputCel").type="hidden";
+        } else if(metodoSelecionado == "cpf"){
+            document.getElementById("inputNome").type="hidden";
+            document.getElementById("inputCPF").type="text";
+            document.getElementById("inputCel").type="hidden";
+        } else{
+            document.getElementById("inputNome").type="hidden";
+            document.getElementById("inputCPF").type="hidden";
+            document.getElementById("inputCel").type="text";
+        }
+    }
+}
+
+function clear(){
+
+}
+
 function getRequest() {
     var ajax;
     if (window.XMLHttpRequest) {
@@ -10,7 +38,7 @@ function getRequest() {
 
 function getDados() {
     var request = getRequest();
-    var cpf = document.getElementById("cpf").value;
+    var cpf = document.getElementById("inputCPF").value;
     cpf = cpf.replace(".", "");
     cpf = cpf.replace(".", "");
     cpf = cpf.replace("-", "");
@@ -18,9 +46,10 @@ function getDados() {
     request.open("GET", url, true);
     request.onreadystatechange = function () {
         if (request.readyState < 4) {
+            document.getElementById("divDados").innerHTML = "Dados não encontrados";
             document.getElementById("divDados").innerHTML = cpf;
         } else if (request.readyState == 4 && request.status == 404) {
-            document.getElementById("divDados").innerHTML = "Dados não encontrados.";
+            document.getElementById("divDados").innerHTML = "Erro 404. Página não encontrada.";
         } else if (request.readyState == 4 && request.status == 200) {
             var cliente = JSON.parse(request.responseText);
             document.getElementById("divDados").innerHTML =
@@ -45,8 +74,9 @@ function getDados() {
 }
 
 $('document').ready(function (e) {
-    $('#cpf').mask("999.999.999-99");
-    $(this.cliente.email).mask("A*****A@A****A.AAA");
+    $('.ui.dropdown').dropdown();
+    $('#inputCPF').mask("999.999.999-99");
+
         $('.searchForm').hide().transition({
             debug: true,
             animation: 'fly left',
@@ -60,6 +90,8 @@ $('document').ready(function (e) {
             duration: 1000,
             interval: 200
         });
+
+
 
     }
 )
--- ProjetoMGov/index.php
+++ ProjetoMGov/index.php
@@ -23,24 +23,43 @@
         </a>
     </div>
     <div class="container">
+        <h1 id="teste"></h1>
         <div class="pusher">
             <h1 class=""ui header">Buscar cliente</h1>
             <div class="ui horizontal divider"><i class="search icon"></i></div>
-            <div class="ui grid">
-                <div class="sixteen wide mobile sixteen wide tablet eight wide computer column">
-                    <div class="ui search searchForm">
-                        <h3 class="ui header">Busque pelo CPF:</h3>
-                        <div class="ui icon input">
-                            <input class="fixed prompt" type="text" id="cpf" name="cpf" placeholder="Informe o CPF">
-                            <i class="search icon"></i>
+            <div class="ui middle aligned center aligned grid">
+                <div class="column" style="max-width: 650px;">
+                    <h2 class="ui header">
+                        <div class="content">
+                            Selecione um meio de busca
                         </div>
-                        <div class="results"></div>
-                        <button onclick="getDados()" class="ui teal button">Buscar</button>
+                    </h2>
+                    <div class="ui large form">
+                        <div class="ui stacked segment">
+                            <div class="field">
+                                <select name="metodos" class="ui fluid search dropdown" onchange="buscarMetodo()" id="busca">
+                                    <option value="">Buscar</option>
+                                    <option value="nome">Nome</option>
+                                    <option value="cpf">CPF</option>
+                                    <option value="cel">Celular</option>
+                                </select>
+                            </div>
+                            <div class="field">
+                                    <input type="hidden" id="inputNome" placeholder="Nome Completo">
+                                    <input type="hidden" id="inputCPF" placeholder="CPF">
+                                    <input type="hidden" id="inputCel" placeholder="Celular">
+                            </div>
+                            <div class="field">
+                                <button onclick="getDados()" type="hidden" id="searchButton" class="ui teal button disabled">
+                                    <i class="search icon"></i>Buscar</button>
+                            </div>
+                            <div class="field" id="divDados">
+
+                            </div>
+                        </div>
                     </div>
-                    <div id="divDados"></div>
                 </div>
             </div>
-
         </div>
     </div>
     <?php
